25c4489ff4ca1076f1ebafeb8e446144274ad88a
Dylan <dylan-brodie@live.nmit.ac.nz>
2025-03-02 14:56:00 +1300

    created CoreLogic JS file and started building functions

---
+++ Code/TicTacToeCoreLogic.js
@@ -0,0 +1,19 @@
+
+//variables
+let playerTurn = true;
+let gameTurn = 0;
+
+//clear the board and reset the game
+function reset(){
+
+}
+
+//turn logic
+function turn(){
+
+}
+
+//call at the end of the turn to see if the player has won
+function checkWinConditions(){
+
+}
