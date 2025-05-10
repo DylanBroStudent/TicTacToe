f4dc3b7f010cc2b28b2a33f34e53ac06ace60ea6
Dylan <dylan-brodie@live.nmit.ac.nz>
2025-05-11 11:16:30 +1200

    ES6 and winConditions

--- Code/TicTacToeCoreLogic.js
+++ Code/TicTacToeCoreLogic.js
@@ -1,7 +1,20 @@
 
 //variables
+
 let playerTurn = true
 let gameTurn = 0
+
+let gameState = [ ]
+
+
+
+// INDEX MAP    INPUT MAP
+// 0 | 1 | 2    1 | 2 | 3
+// ---------    ---------
+// 3 | 4 | 5    4 | 5 | 6
+// ---------    ---------
+// 6 | 7 | 8    7 | 8 | 9
+
 //Square state - 0 = empty, 1 = X, 2 = 0
 let gameSquares = [
     {squareNo:0, state:0},
@@ -12,12 +25,24 @@ let gameSquares = [
     {squareNo:5, state:0},
     {squareNo:6, state:0},
     {squareNo:7, state:0},
-    {squareNo:8, state:0},
-    {squareNo:9, state:0}
+    {squareNo:8, state:0}
+]
+
+let winConditions = [
+    [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,5],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
 ]
 
 //clear the board and reset the game
-function reset(){
+const reset = () =>{
     playerTurn = true
     gameTurn = 0
     gameSquares.forEach(square => {
@@ -27,11 +52,7 @@ function reset(){
 }
 
 //turn logic
-function turn(){
-
-}
+const turn = () => {}
 
 //call at the end of the turn to see if the player has won
-function checkWinConditions(){
-    
-}
+const checkWinConditions = () => {}
